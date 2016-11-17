a35ed5a6cb4da820e5d82a196965b5ad7b7fe0d6
junk151516 <junk151516@gmail.com>
2016-11-16 19:00:12 -0500

    add super reporte

--- hacebrep/js/index.js
+++ hacebrep/js/index.js
@@ -4,9 +4,15 @@ angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
   $mdIconProvider
     .iconSet('communication', 'img/icons/sets/communication-icons.svg', 24);
 })
-.controller('AppCtrl', function($scope) {
+.controller('AppCtrl', function($scope, $location) {
+	
+	
     var imagePath = 'img/list/60.jpeg';
 
+	$scope.go = function ( path ) {
+		$location.path( path );
+	};
+	
     $scope.phones = [
       {
         type: 'Home',
@@ -45,42 +51,111 @@ angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
         what: 'Proyecto Antioquia',
         who: 'T2',
         when: '3:08PM',
-        notes: "Marleni Florez 3233336318",
-		links: "genReporte.html"
-		
+        notes: "Marleni Florez",
+		links: "t2.html",
+		city : "Sincelejo"
       },
       {
         face : imagePath,
         what: 'Proyecto Antioquia',
         who: 'T3',
         when: '3:08PM',
-        notes: "walter alvarez 3017319548",
-		links: "genReporte.html"
+        notes: "walter alvarez",
+		links: "t3.html",
+		city : "Sincelejo"
       },
       {
         face : imagePath,
-        what: 'Brunch this weekend?',
-        who: 'Min Li Chan',
+        what: 'Proyecto Antioquia',
+        who: 'T4',
         when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands",
-        links: "genReporte.html"		
+        notes: "Jhon David Vandelamar",
+		links: "t4.html",
+		city : "Cartagena"	
+      },
+      {
+      face : imagePath,
+        what: 'Proyecto Antioquia',
+        who: 'T5',
+        when: '3:08PM',
+        notes: "Luz Arteaga",
+		links: "t5.html",
+		city : "Barranquilla"
       },
       {
         face : imagePath,
-        what: 'Brunch this weekend?',
-        who: 'Min Li Chan',
+        what: 'conservador C150L (Super Eco)',
+        who: 'T6',
         when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands",
-		links: "genReporte.html"
+        notes: "Alexy del Carmen Payares",
+		links: "t6.html",
+		city : "Barranquilla"
       },
       {
-        face : imagePath,
-        what: 'Brunch this weekend?',
-        who: 'Min Li Chan',
+      face : imagePath,
+        what: 'Proyecto Antioquia',
+        who: 'T7',
         when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands",
-		links: "genReporte.html"
+        notes: "Delsy Sierra Alvarez",
+		links: "t7.html",
+		city : "Sincelejo"
       },
+	  {
+      face : imagePath,
+        what: 'Antioquia Bajo régimen',
+        who: 'T8',
+        when: '3:08PM',
+        notes: "Haceb Chequeos",
+		links: "t8.html",
+		city : "Medellin"
+      },
+      {
+      face : imagePath,
+        what: 'Proyecto Antioquia',
+        who: 'T9',
+        when: '3:08PM',
+        notes: "Linet Hernandez",
+		links: "t9.html",
+		city : "Barranquilla"
+      },
+      {
+      face : imagePath,
+        what: 'conservador C150L (Super Eco)',
+        who: 'T10',
+        when: '3:08PM',
+        notes: "Yuleivy Judith Meza Payares",
+		links: "t10.html",
+		city : "Barranquilla"
+      },
+      {
+      face : imagePath,
+        what: 'Proyecto Antioquia',
+        who: 'T11',
+        when: '3:08PM',
+        notes: "Angie Paola Valencia",
+		links: "t11.html",
+		city : "Cartagena"
+      },
+      {
+      face : imagePath,
+        what: 'Proyecto Antioquia',
+        who: 'T12',
+        when: '3:08PM',
+        notes: "Gladis Gutierrez de Gomez",
+		links: "t12.html",
+		city : "Barranquilla"
+      },
+      {
+      face : imagePath,
+        what: 'Proyecto Antioquia',
+        who: 'T13',
+        when: '3:08PM',
+        notes: "Graciela Cecilia Arrieta Sierra",
+		links: "t13.html",
+		city : "Sincelejo"
+      }
+	  
+	  
     ];
 });
 
